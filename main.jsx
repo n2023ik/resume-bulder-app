e09ee03cad32f57881b111b68ba32cbcae51eb1a
Nikhil Pandey <pandeynikhil429@gmail.com>
2025-10-04 02:55:10 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import ResumeBuilder from './app.jsx'
+import './app.css'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
